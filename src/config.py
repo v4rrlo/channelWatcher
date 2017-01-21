c3b391dd7590fcf02d559360a467f94554439c0c
mrzeznikowski <m.rzeznikowski@millionmonkeys.pl>
2017-01-21 02:06:48 +0100

    im dumb

--- src/config.py
+++
@@ -1,5 +0,0 @@
-# Bunch of constants
-API_KEY = '4p680e8nfzrai4py2gqd23trqvz4nj'
-TWITCH_USER = 'varrlo'  # your username on twitch, needed to get list of followed users
-REFRESH_TIME = 300      # after how many seconds do we check statuses again
-NUMBER_OF_THREADS = 10
